88e8a63ccb1636f4eb0e44aec5d03ee7c86a9126
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 15:12:25 -0400

    redux

---
+++ src/redux/slice.js
@@ -0,0 +1,11 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import { postAssistanceDate } from "./actions";
+
+// export const Slice = createSlice({
+//   name: "root",
+//   initialState: {},
+//   reducers: {},
+// });
+
+// export const {} = Slice.actions;
+// export default Slice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import root from "./slice";
+
+// export default configureStore({
+//   reducer: {
+//     root,
+//   },
+// });
