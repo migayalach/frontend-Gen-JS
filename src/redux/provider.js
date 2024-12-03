73278e9436e89d69d7b24e482c904ef8550c25f9
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 17:38:06 -0400

    src/redux/provider.js

--- src/redux/provider.js
+++ src/redux/provider.js
@@ -1,9 +1,9 @@
-// "use client";
-// import { Provider } from "react-redux";
-// import store from "./store";
+"use client";
+import { Provider } from "react-redux";
+import store from "./store";
 
-// const Providers = ({ children }) => {
-//   return <Provider store={store}>{children}</Provider>;
-// };
+const Providers = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
 
-// export default Providers;
+export default Providers;
