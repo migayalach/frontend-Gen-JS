266a1bc9caf472a21e451abc95294a0b6a82f711
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 17:37:50 -0400

    src/redux/actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,13 @@
-// import axios from "axios";
-// const URL = "http://localhost:3001/academy";
-// import {} from "./slice";
+import axios from "axios";
+import { getAllUsers } from "./slice";
+require("dotenv").config();
+const URL = process.env.URL;
+
+export const getUsersAll = () => {
+  return async function (dispatch) {
+    try {
+      const data = await axios.post(`${URL}/users`).data;
+      return dispatch(getAllUsers(data));
+    } catch (error) {}
+  };
+};
