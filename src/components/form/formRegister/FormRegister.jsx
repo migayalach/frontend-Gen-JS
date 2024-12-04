d3b2d3184e5063a1210259e30d54fe40e5269c6c
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 18:52:10 -0400

    src/components/form/formRegister/FormRegister.jsx

--- src/components/form/formRegister/FormRegister.jsx
+++ src/components/form/formRegister/FormRegister.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { Button, Checkbox, Form, Input } from "antd";
-
-const onFinish = (values) => {
-  console.log("Success:", values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
+import React, { useState } from "react";
+import { Button, Form, Input } from "antd";
+import { postUsers } from "@/redux/actions";
+import { useDispatch } from "react-redux";
 
 function FormRegister() {
+  const dispatch = useDispatch();
+  const [data, setData] = useState({
+    nameUser: "",
+    lastNameUser: "",
+    emailUser: "",
+    passwordUser: "",
+  });
+
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onFinish = () => {
+    dispatch(postUsers(data));
+  };
+
   return (
     <div>
       <Form
@@ -17,7 +31,7 @@ function FormRegister() {
           span: 8,
         }}
         wrapperCol={{
-          span: 16,
+          span: 14,
         }}
         style={{
           maxWidth: 600,
@@ -26,59 +40,54 @@ function FormRegister() {
           remember: true,
         }}
         onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
         <Form.Item
           label="Username"
-          name="username"
           rules={[
             {
               required: true,
-              message: "Please input your username!",
+              message: "Please input your nameUser!",
             },
           ]}
         >
-          <Input />
+          <Input name="nameUser" onChange={handleChange} />
         </Form.Item>
 
         <Form.Item
           label="Lastname"
-          name="lastname"
           rules={[
             {
               required: true,
-              message: "Please input your lastname!",
+              message: "Please input your lastNameUser!",
             },
           ]}
         >
-          <Input />
+          <Input name="lastNameUser" onChange={handleChange} />
         </Form.Item>
 
         <Form.Item
           label="Email"
-          name="email"
           rules={[
             {
               required: true,
-              message: "Please input your email!",
+              message: "Please input your emailUser!",
             },
           ]}
         >
-          <Input />
+          <Input name="emailUser" onChange={handleChange} />
         </Form.Item>
 
         <Form.Item
           label="Password"
-          name="password"
           rules={[
             {
               required: true,
-              message: "Please input your password!",
+              message: "Please input your passwordUser!",
             },
           ]}
         >
-          <Input.Password />
+          <Input.Password name="passwordUser" onChange={handleChange} />
         </Form.Item>
 
         <Form.Item label={null}>
