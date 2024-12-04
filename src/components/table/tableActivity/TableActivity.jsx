dbb755d956c581fc983a05232705c3be69c03dc5
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:59:10 -0400

    src/components/table/tableActivity/TableActivity.jsx

--- src/components/table/tableActivity/TableActivity.jsx
+++ src/components/table/tableActivity/TableActivity.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Table, Tag } from "antd";
 
-function TableActivity() {
+function TableActivity({ list }) {
+  const columns = [
+    {
+      title: "N°",
+      dataIndex: "numberItem",
+      key: "numberItem",
+    },
+    {
+      title: "Name",
+      dataIndex: "nameUser",
+      key: "nameUser",
+    },
+    {
+      title: "Type activity",
+      dataIndex: "nameEntryExit",
+      key: "nameEntryExit",
+      render: (nameEntryExit) => {
+        let color = nameEntryExit === "entrada" ? "success" : "error";
+        let text = nameEntryExit === "entrada" ? "Login" : "Out";
+        return (
+          <Tag color={color}>
+            <p>{text}</p>
+          </Tag>
+        );
+      },
+    },
+    {
+      title: "Time",
+      dataIndex: "timeEntryExit",
+      key: "timeEntryExit",
+    },
+  ];
+
+  const dataMap = (data) => {
+    return data?.map(({ nameUser, nameEntryExit, timeEntryExit }, index) => ({
+      key: index,
+      numberItem: index + 1,
+      nameUser,
+      nameEntryExit,
+      timeEntryExit,
+    }));
+  };
+
   return (
-    <div>TableActivity</div>
-  )
+    <div>
+      <Table columns={columns} dataSource={dataMap(list)} pagination={true} />
+    </div>
+  );
 }
 
-export default TableActivity
+export default TableActivity;
