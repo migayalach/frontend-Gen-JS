40ab478eee117bf2827bbbeccc34b3c8b6ca0874
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:58:24 -0400

    src/app/user/page.jsx

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { getUsersAll } from "@/redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import TableUser from "@/components/table/tableUser/TableUser";
 
 function page() {
+  const dispatch = useDispatch();
+  const selectUsers = useSelector(({ root }) => root.users);
+
+  useEffect(() => {
+    dispatch(getUsersAll());
+  }, []);
+
   return (
-    <div>USER</div>
-  )
+    <div>
+      <h1>List Users</h1>
+      <TableUser list={selectUsers} />
+    </div>
+  );
 }
 
-export default page
+export default page;
