c5f13da4ef45dde48aac6ed3c1212026211ec037
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 14:00:03 -0400

    src/redux/actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,104 @@
 import axios from "axios";
-import { getAllUsers } from "./slice";
+import {
+  getAllUsers,
+  getAllProducts,
+  getIdProduct,
+  removeProduct,
+  clearState,
+  clearAux,
+  getAllAudit,
+  errorResponse,
+} from "./slice";
 require("dotenv").config();
-const URL = process.env.URL;
+const URL = `http://localhost:3001/api`;
 
+// *USER
 export const getUsersAll = () => {
   return async function (dispatch) {
     try {
-      const data = await axios.post(`${URL}/users`).data;
+      const data = (await axios.get(`${URL}/user`)).data;
       return dispatch(getAllUsers(data));
-    } catch (error) {}
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+// *AUDIT-USER
+export const getAuditUser = () => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/entryExit`)).data;
+      return dispatch(getAllAudit(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+// *AUDIT-PRODUCT
+export const getAuditProduct = () => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/auditProduct`)).data;
+      return dispatch(getAllAudit(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+// *PRODUCT
+export const getProductsll = () => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/product`)).data;
+      return dispatch(getAllProducts(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+export const deleteProduct = (idProduct) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.delete(`${URL}/product/${idProduct}`)).data;
+      return dispatch(removeProduct(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+export const getProductId = (idProduct) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/product/${idProduct}`)).data;
+      return dispatch(getIdProduct(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+// *CLEAR DATA
+export const stateClear = () => {
+  return function (dispatch) {
+    try {
+      return dispatch(clearState());
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
+  };
+};
+
+export const auxClear = () => {
+  return function (dispatch) {
+    try {
+      return dispatch(clearAux());
+    } catch (error) {
+      return dispatch(errorResponse(error.response.data));
+    }
   };
 };
