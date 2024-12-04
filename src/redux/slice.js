5679325556fda831b223e4f01736bae1ab5722b4
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 14:00:32 -0400

    src/redux/slice.js

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -4,14 +4,58 @@ export const Slice = createSlice({
   name: "root",
   initialState: {
     users: [],
+    products: [],
+    audit: [],
+    aux: {},
+    state: "",
+    error: null,
   },
   reducers: {
     // *USERS
     getAllUsers: (state, action) => {
       state.users = action.payload;
     },
+
+    // *PRODUCTS
+    getAllProducts: (state, action) => {
+      state.products = action.payload;
+    },
+
+    getIdProduct: (state, action) => {
+      state.aux = action.payload;
+      state.products = [];
+    },
+
+    removeProduct: (state, action) => {
+      state.state = action.payload;
+    },
+
+    clearState: (state, action) => {
+      state.state = "";
+    },
+
+    clearAux: (state, action) => {
+      state.aux = {};
+    },
+
+    getAllAudit: (state, action) => {
+      state.audit = action.payload;
+    },
+
+    errorResponse: (state, action) => {
+      state.error = action.payload;
+    },
   },
 });
 
-export const { getAllUsers } = Slice.actions;
+export const {
+  getAllUsers,
+  getAllProducts,
+  getIdProduct,
+  removeProduct,
+  clearState,
+  clearAux,
+  getAllAudit,
+  errorResponse,
+} = Slice.actions;
 export default Slice.reducer;
