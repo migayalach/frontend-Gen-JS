fa70841a7f805bde35701b71a9004bf471f3ad3e
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 14:00:50 -0400

    src/components/floatOption/

---
+++ src/components/floatOption/createProduct/CreateProduct.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { FileAddOutlined } from "@ant-design/icons";
+import { FloatButton, Modal } from "antd";
+import FormProduct from "@/components/form/formProduct/FormProduct";
+
+function CreateProduct({ flag }) {
+  const [modal, setModal] = useState(false);
+
+  const showModal = () => {
+    setModal(true);
+  };
+
+  const handleOk = () => {
+    setModal(false);
+  };
+  const handleCancel = () => {
+    setModal(false);
+  };
+
+  return (
+    <>
+      <FloatButton
+        icon={<FileAddOutlined />}
+        type="primary"
+        style={{
+          insetInlineEnd: 24,
+        }}
+        onClick={() => showModal()}
+      />
+
+      {modal && (
+        <Modal
+          title="Create product"
+          open={modal}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          {flag === "Create-product" && <FormProduct flag={flag} />}
+          {flag === "Edit-product" && <FormProduct flag={flag} />}
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default CreateProduct;
