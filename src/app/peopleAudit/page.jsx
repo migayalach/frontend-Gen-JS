59172097589cd4fca0717bb0eb68d642aac2aeb6
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:56:49 -0400

    src/app/peopleAudit/page.jsx

--- src/app/peopleAudit/page.jsx
+++ src/app/peopleAudit/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { getAuditUser } from "@/redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import TableActivity from "@/components/table/tableActivity/TableActivity";
 
 function page() {
+  const dispatch = useDispatch();
+  const selectAudit = useSelector(({ root }) => root.audit);
+
+  useEffect(() => {
+    dispatch(getAuditUser());
+  }, []);
+
   return (
-    <div>people</div>
-  )
+    <div>
+      <h1>List People activity</h1>
+      <TableActivity list={selectAudit} />
+    </div>
+  );
 }
 
-export default page
+export default page;
