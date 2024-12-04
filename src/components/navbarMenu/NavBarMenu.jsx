47a4858e770b4f231ba1e9b9f2fc731ee365ca95
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 18:53:17 -0400

    src/components/navbarMenu/NavBarMenu.jsx

--- src/components/navbarMenu/NavBarMenu.jsx
+++ src/components/navbarMenu/NavBarMenu.jsx
@@ -11,59 +11,98 @@ import {
 import { Menu } from "antd";
 import Link from "next/link";
 import ModalLogin from "../modal/modalLogin/ModalLogin";
-
-const items = [
-  {
-    label: <Link href="/">Home</Link>,
-    key: "home",
-    icon: <HomeOutlined />,
-  },
-  {
-    label: <Link href="/user">Users</Link>,
-    key: "user",
-    icon: <UsergroupDeleteOutlined />,
-  },
-  {
-    label: <Link href="/product">Products</Link>,
-    key: "products",
-    icon: <ProductOutlined />,
-  },
-  {
-    label: "Audit",
-    key: "SubMenu",
-    icon: <UnorderedListOutlined />,
-    children: [
-      {
-        type: "group",
-        label: "Audit",
-        children: [
-          {
-            label: <Link href="/peopleAudit">Activity</Link>,
-            key: "peopleAudit",
-          },
-          {
-            label: <Link href="/productAudit">Products</Link>,
-            key: "productAudit",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Login",
-    key: "login",
-    icon: <LoginOutlined />,
-  },
-  {
-    label: "Out",
-    key: "out",
-    icon: <CloseSquareOutlined />,
-  },
-];
+import { useSelector, useDispatch } from "react-redux";
+import { auditUserOut } from "@/redux/actions";
 
 function NavBarMenu() {
   const [current, setCurrent] = useState("home");
+  const dispatch = useDispatch();
   const [modalShow, setModalShow] = useState(false);
+  const selectLogin = useSelector(({ root }) => root.login);
+  const levelAccess = selectLogin?.access;
+
+  const handleExit = () => {
+    dispatch(
+      auditUserOut({ idUser: selectLogin?.data?.idUser, idEntryExit: 2 })
+    );
+  };
+
+  const items = [
+    {
+      label: <Link href="/">Home</Link>,
+      key: "home",
+      icon: <HomeOutlined />,
+    },
+    ...(levelAccess
+      ? [
+          {
+            label: <Link href="/user">Users</Link>,
+            key: "user",
+            icon: <UsergroupDeleteOutlined />,
+          },
+        ]
+      : []),
+    {
+      label: <Link href="/product">Products</Link>,
+      key: "products",
+      icon: <ProductOutlined />,
+    },
+
+    ...(levelAccess
+      ? [
+          {
+            label: "Audit",
+            key: "SubMenu",
+            icon: <UnorderedListOutlined />,
+            children: [
+              {
+                type: "group",
+                label: "Audit",
+                children: [
+                  {
+                    label: <Link href="/peopleAudit">Activity</Link>,
+                    key: "peopleAudit",
+                  },
+                  {
+                    label: <Link href="/productAudit">Products</Link>,
+                    key: "productAudit",
+                  },
+                ],
+              },
+            ],
+          },
+        ]
+      : []),
+
+    ...(!levelAccess
+      ? [
+          {
+            label: "Login",
+            key: "login",
+            icon: <LoginOutlined />,
+          },
+        ]
+      : []),
+
+    ...(levelAccess
+      ? [
+          {
+            label: (
+              <a
+                href="/"
+                onClick={() => {
+                  handleExit();
+                }}
+              >
+                Out
+              </a>
+            ),
+            key: "out",
+            icon: <CloseSquareOutlined />,
+          },
+        ]
+      : []),
+  ];
 
   const onClick = (event) => {
     setCurrent(event.key);
