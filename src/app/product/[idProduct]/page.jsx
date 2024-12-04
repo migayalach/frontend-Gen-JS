ee0b1bdb1813995f1e826f14ea939bd3729265ee
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 16:07:59 -0400

    src/app/product/[idProduct]/page.jsx

--- src/app/product/[idProduct]/page.jsx
+++ src/app/product/[idProduct]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { getIdProduct, auxClear } from "@/redux/actions";
+import CreateProduct from "@/components/floatOption/createProduct/CreateProduct";
 import { useSelector } from "react-redux";
 import "./styleProduct.css";
 
@@ -11,50 +12,57 @@ import "./styleProduct.css";
 function page({ params }) {
   const selectAux = useSelector(({ root }) => root.aux);
   return (
-    <div className="body-container">
-      <section className="container-card">
-        <article>
-          <div className="image-container">
-            <img
-              className="image-photo-head"
-              src={selectAux?.urlProduct}
-              alt={selectAux?.nameProduct}
-            />
-          </div>
-          <header>
-            <div className="container-span-header">
-              <span className="text-title" style={{ background: "#f3c05b" }}>
-                Price: $ {selectAux?.priceProduct}
-              </span>
-              <span className="text-title" style={{ background: "#d5f2e3" }}>
-                State:
-                {selectAux?.stateProduct === 0 ? "Out of stock" : "In stock"}
-              </span>
+    <>
+      <div className="body-container">
+        <section className="container-card">
+          <article>
+            <div className="image-container">
+              <img
+                className="image-photo-head"
+                src={selectAux?.urlProduct}
+                alt={selectAux?.nameProduct}
+              />
             </div>
-            <p
-              style={{ fontSize: "30px", fontWeight: "bold", marginTop: "5px" }}
-            >
-              {selectAux?.nameProduct}
+            <header>
+              <div className="container-span-header">
+                <span className="text-title" style={{ background: "#f3c05b" }}>
+                  Price: $ {selectAux?.priceProduct}
+                </span>
+                <span className="text-title" style={{ background: "#d5f2e3" }}>
+                  State:
+                  {selectAux?.stateProduct === 0
+                    ? " Out of stock"
+                    : " In stock"}
+                </span>
+              </div>
+              <p
+                style={{
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                  marginTop: "5px",
+                }}
+              >
+                {selectAux?.nameProduct}
+              </p>
+            </header>
+            <p className="description-text">
+              Date product: {selectAux?.dateIntroProduct?.slice(0, 10)}
             </p>
-          </header>
-          <p className="description-text">
-            Date product: {selectAux?.dateIntroProduct?.slice(0, 10)}
-          </p>
-          <p className="description-text">Code: {selectAux?.codeProduct}</p>
-          <p className="description-text">Size: {selectAux?.sizeProduct}</p>
+            <p className="description-text">Code: {selectAux?.codeProduct}</p>
+            <p className="description-text">Size: {selectAux?.sizeProduct}</p>
 
-          <footer>
-            {/* <img
-              className="profile-image"
-              src=""
-              alt="Profile photo of Patricia Lopez"
-            /> */}
-            <span>Stock: {selectAux?.stockProduct}</span>
-            <span>Made: {selectAux?.madeProduct}</span>
-          </footer>
-        </article>
-      </section>
-    </div>
+            <footer>
+              <span>Stock: {selectAux?.stockProduct}</span>
+              <span>Made: {selectAux?.madeProduct}</span>
+            </footer>
+          </article>
+        </section>
+      </div>
+
+      <div>
+        <CreateProduct flag="Edit-product" />
+      </div>
+    </>
   );
 }
 
