e45471ba5d9ddbe7ea077d0a4b12f4f9d6ac73f7
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:57:15 -0400

    src/app/product/[idProduct]/page.jsx

--- src/app/product/[idProduct]/page.jsx
+++ src/app/product/[idProduct]/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { getIdProduct, auxClear } from "@/redux/actions";
+import { useSelector } from "react-redux";
+import "./styleProduct.css";
 
-function page() {
+// idProduct
+//
+//
+
+function page({ params }) {
+  const selectAux = useSelector(({ root }) => root.aux);
   return (
-    <div>product ID</div>
-  )
+    <div className="body-container">
+      <section className="container-card">
+        <article>
+          <div className="image-container">
+            <img
+              className="image-photo-head"
+              src={selectAux?.urlProduct}
+              alt={selectAux?.nameProduct}
+            />
+          </div>
+          <header>
+            <div className="container-span-header">
+              <span className="text-title" style={{ background: "#f3c05b" }}>
+                Price: $ {selectAux?.priceProduct}
+              </span>
+              <span className="text-title" style={{ background: "#d5f2e3" }}>
+                State:
+                {selectAux?.stateProduct === 0 ? "Out of stock" : "In stock"}
+              </span>
+            </div>
+            <p
+              style={{ fontSize: "30px", fontWeight: "bold", marginTop: "5px" }}
+            >
+              {selectAux?.nameProduct}
+            </p>
+          </header>
+          <p className="description-text">
+            Date product: {selectAux?.dateIntroProduct?.slice(0, 10)}
+          </p>
+          <p className="description-text">Code: {selectAux?.codeProduct}</p>
+          <p className="description-text">Size: {selectAux?.sizeProduct}</p>
+
+          <footer>
+            {/* <img
+              className="profile-image"
+              src=""
+              alt="Profile photo of Patricia Lopez"
+            /> */}
+            <span>Stock: {selectAux?.stockProduct}</span>
+            <span>Made: {selectAux?.madeProduct}</span>
+          </footer>
+        </article>
+      </section>
+    </div>
+  );
 }
 
-export default page
+export default page;
