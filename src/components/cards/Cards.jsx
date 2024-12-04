d7bf8c5cca5c614e09ccf2ae4ecc6fa2d27d0dd9
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:58:45 -0400

    src/components/cards/Cards.jsx

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Card } from "antd";
+const { Meta } = Card;
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { deleteProduct, getProductId } from "@/redux/actions";
+import { useDispatch } from "react-redux";
+import Link from "next/link";
+
+function Cards({ list }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = (idProduct) => {
+    dispatch(deleteProduct(idProduct));
+  };
+
+  const handleUpdate = (idProduct) => {
+    dispatch(getProductId(idProduct));
+  };
+
+  return (
+    <>
+      {list.map(
+        ({ idProduct, nameProduct, codeProduct, priceProduct, urlProduct }) => (
+          <Card
+            key={idProduct}
+            hoverable
+            style={{
+              width: 300,
+              margin: "10px",
+            }}
+            cover={
+              <img
+                alt={nameProduct}
+                className="w-full h-full object-cover"
+                src={
+                  urlProduct ||
+                  "https://res.cloudinary.com/dqgcyonb9/image/upload/v1733325904/samples/k9uukxe6qgwnyqkyheye.png"
+                }
+              />
+            }
+            actions={[
+              <DeleteOutlined
+                key="Delete"
+                onClick={() => handleDelete(idProduct)}
+                style={{ color: "#da2c38", fontSize: "20px" }}
+              />,
+              <Link
+                onClick={() => handleUpdate(idProduct)}
+                href={`/product/${idProduct}`}
+                key="Edit"
+              >
+                <EditOutlined style={{ color: "#2a9d8f", fontSize: "20px" }} />
+              </Link>,
+            ]}
+          >
+            <div>
+              <div className="flex justify-between items-center">
+                <h2 className="text-2xl font-bold">{nameProduct}</h2>
+                <span className="text-xl font-semibold text-[#001d3d]">
+                  ${priceProduct}
+                </span>
+              </div>
+              <p className="text-gray-600 mt-4 leading-relaxed">
+                Code: {codeProduct}
+              </p>
+            </div>
+          </Card>
+        )
+      )}
+    </>
+  );
+}
+
+export default Cards;
