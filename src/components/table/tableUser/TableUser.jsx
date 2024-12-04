07925d9ec523b4af61d023d47d739d8f30c99a55
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:59:53 -0400

    src/components/table/tableUser/TableUser.jsx

--- src/components/table/tableUser/TableUser.jsx
+++ src/components/table/tableUser/TableUser.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Table } from "antd";
 
-function TableUser() {
+function TableUser({ list }) {
+  const columns = [
+    {
+      title: "N°",
+      dataIndex: "numberItem",
+      key: "numberItem",
+    },
+    {
+      title: "Name",
+      dataIndex: "nameUser",
+      key: "nameUser",
+    },
+    {
+      title: "LastName",
+      dataIndex: "lastNameUser",
+      key: "lastNameUser",
+    },
+    {
+      title: "Email",
+      dataIndex: "emailUser",
+      key: "emailUser",
+    },
+  ];
+
+  const dataMap = (data) => {
+    return data?.map(({ nameUser, lastNameUser, emailUser }, index) => ({
+      key: index,
+      numberItem: index + 1,
+      nameUser,
+      lastNameUser,
+      emailUser,
+    }));
+  };
+
   return (
-    <div>TableUser</div>
-  )
+    <div>
+      <Table columns={columns} dataSource={dataMap(list)} pagination={true} />
+    </div>
+  );
 }
 
-export default TableUser
+export default TableUser;
