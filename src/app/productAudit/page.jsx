a8e5fcd23a1c6b69bd940eb5ad607c073bf9d4e1
migayalach <ayalachavezmiguel@gmail.com>
2024-12-04 13:57:56 -0400

    src/app/productAudit/page.jsx

--- src/app/productAudit/page.jsx
+++ src/app/productAudit/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { getAuditProduct } from "@/redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import TableProductsAudit from "@/components/table/tableProductsAudit/TableProductsAudit";
 
 function page() {
+  const dispatch = useDispatch();
+  const selectAudit = useSelector(({ root }) => root.audit);
+
+  useEffect(() => {
+    dispatch(getAuditProduct());
+  }, []);
+
   return (
-    <div>product Audit</div>
-  )
+    <div>
+      <h1>List products audit</h1>
+      <TableProductsAudit list={selectAudit} />
+    </div>
+  );
 }
 
-export default page
+export default page;
