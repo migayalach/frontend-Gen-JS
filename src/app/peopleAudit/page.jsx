6d0b24c46a6325dd54ea6074403480804e429cf0
migayalach <ayalachavezmiguel@gmail.com>
2024-12-09 10:56:40 -0400

    src/app/peopleAudit/page.jsx

--- src/app/peopleAudit/page.jsx
+++ src/app/peopleAudit/page.jsx
@@ -3,10 +3,12 @@ import React, { useEffect } from "react";
 import { getAuditUser } from "@/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import TableActivity from "@/components/table/tableActivity/TableActivity";
+import Pag from "@/components/pagination/Pag";
 
 function page() {
   const dispatch = useDispatch();
   const selectAudit = useSelector(({ root }) => root.audit);
+  const selectInfo = useSelector(({ root }) => root?.info);
 
   useEffect(() => {
     dispatch(getAuditUser());
@@ -14,8 +16,15 @@ function page() {
 
   return (
     <div>
-      <h1>List People activity</h1>
-      <TableActivity list={selectAudit} />
+      <header>
+        <h1>List People activity</h1>
+      </header>
+      <main>
+        <TableActivity list={selectAudit} />
+      </main>
+      <footer>
+        <Pag info={selectInfo} flag="people-audit" />
+      </footer>
     </div>
   );
 }
