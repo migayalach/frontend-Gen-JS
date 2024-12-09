a0d4714160b73f449f810eb9d90e4a02311b839b
migayalach <ayalachavezmiguel@gmail.com>
2024-12-09 10:56:53 -0400

    src/app/productAudit/page.jsx

--- src/app/productAudit/page.jsx
+++ src/app/productAudit/page.jsx
@@ -3,10 +3,12 @@ import React, { useEffect } from "react";
 import { getAuditProduct } from "@/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import TableProductsAudit from "@/components/table/tableProductsAudit/TableProductsAudit";
+import Pag from "@/components/pagination/Pag";
 
 function page() {
   const dispatch = useDispatch();
   const selectAudit = useSelector(({ root }) => root.audit);
+  const selectInfo = useSelector(({ root }) => root?.info);
 
   useEffect(() => {
     dispatch(getAuditProduct());
@@ -14,8 +16,15 @@ function page() {
 
   return (
     <div>
-      <h1>List products audit</h1>
-      <TableProductsAudit list={selectAudit} />
+      <header>
+        <h1>List products audit</h1>
+      </header>
+      <main>
+        <TableProductsAudit list={selectAudit} />
+      </main>
+      <footer>
+        <Pag info={selectInfo} flag="product-audit" />
+      </footer>
     </div>
   );
 }
