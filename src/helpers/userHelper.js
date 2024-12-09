a90c0e303cecec2b89d94da1e6b7ffee0f898353
migayalach <ayalachavezmiguel@gmail.com>
2024-12-08 22:11:33 -0400

    src/helpers/

---
+++ src/helpers/userHelper.js
@@ -0,0 +1,8 @@
+export const clearDataRegister = (data) => {
+  return {
+    emailUser: data.emailUser,
+    lastNameUser: data.lastNameUser,
+    nameUser: data.nameUser,
+    passwordUser: data.passwordUser,
+  };
+};
