279be8a0844e19b67e828bd56213e692ef191cb4
migayalach <ayalachavezmiguel@gmail.com>
2024-12-09 10:25:41 -0400

    src/app/product/page.jsx

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -4,10 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProductsll, stateClear } from "@/redux/actions";
 import Cards from "@/components/cards/Cards";
 import CreateProduct from "@/components/floatOption/createProduct/CreateProduct";
+import Pag from "@/components/pagination/Pag";
 
 function page() {
   const dispatch = useDispatch();
   const selectProducts = useSelector(({ root }) => root.products);
+  const selectInfo = useSelector(({ root }) => root?.info);
   const selectState = useSelector(({ root }) => root.state);
   const selectLogin = useSelector(({ root }) => root.login);
   const levelAccess = selectLogin?.access;
@@ -27,24 +29,34 @@ function page() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center mb-4 mt-2">
-        List products
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        <Cards
-          list={selectProducts}
-          vie={levelAccess}
-          user={selectLogin?.data?.idUser}
-        />
-      </div>
-      {levelAccess && (
-        <div>
-          <CreateProduct
-            flag="Create-product"
+      <header>
+        <h1 className="text-2xl font-bold text-center mb-4 mt-2">
+          List products
+        </h1>
+      </header>
+      <main>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          <Cards
+            list={selectProducts}
+            vie={levelAccess}
             user={selectLogin?.data?.idUser}
           />
         </div>
-      )}
+      </main>
+
+      <section>
+        {levelAccess && (
+          <div>
+            <CreateProduct
+              flag="Create-product"
+              user={selectLogin?.data?.idUser}
+            />
+          </div>
+        )}
+      </section>
+      <footer>
+        <Pag info={selectInfo} flag="products" />
+      </footer>
     </div>
   );
 }
