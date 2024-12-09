42e35c429eb7612ac3db997c3c2293ed43f56a40
migayalach <ayalachavezmiguel@gmail.com>
2024-12-08 22:12:36 -0400

    src/components/modal/modalEdit/

---
+++ src/components/modal/modalEdit/ModalEdit.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { getIdUser, auxClear } from "@/redux/actions";
+import FormRegister from "@/components/form/formRegister/FormRegister";
+
+function ModalEdit({ idData, flag }) {
+  const selectLevel = useSelector(({ root }) => root.level);
+  const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+    dispatch(getIdUser(idData));
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    dispatch(auxClear());
+  };
+
+  return (
+    <>
+      <Button type="primary" danger onClick={showModal}>
+        <EditOutlined />
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="cancel" onClick={handleCancel}>
+            Cancel
+          </Button>,
+        ]}
+      >
+        <FormRegister list={selectLevel} flag={flag} />
+      </Modal>
+    </>
+  );
+}
+
+export default ModalEdit;
