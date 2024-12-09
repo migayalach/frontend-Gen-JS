0c26ea7b9fbdc7b0b39abf6fdff665adb6e1bd49
migayalach <ayalachavezmiguel@gmail.com>
2024-12-09 10:26:31 -0400

    src/components/pagination/Pag.jsx

--- src/components/pagination/Pag.jsx
+++ src/components/pagination/Pag.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { Pagination } from "antd";
-import { getPageUserAll } from "@/redux/actions";
+import { getPageUserAll, getPageProductsAll } from "@/redux/actions";
 import { useDispatch } from "react-redux";
 
-function Pag({ info, access }) {
+function Pag({ info, access, flag }) {
   const dispatch = useDispatch();
   const [current, setCurrent] = useState(1);
   const totalPage = (info?.pages ?? 0) * 10;
 
   const onChange = (page) => {
     setCurrent(page);
-    dispatch(getPageUserAll(access, page));
+    if (flag === "users") {
+      dispatch(getPageUserAll(access, page));
+    } else if (flag === "products") {
+      dispatch(getPageProductsAll(page));
+    }
   };
 
   return (
